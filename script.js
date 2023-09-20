b681275000a2aaf6a2bd8cad98e2da1d59eb5632
Abigail <cinco.de.abby@gmail.com>
2023-09-20 04:32:50 -0400

    added all characters in const

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//character sets
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '1234567890';
+const special = '!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~';
+
+
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
