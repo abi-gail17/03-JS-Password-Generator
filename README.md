b9d4cc9060076b4cd74ff80a0058fdb73ca4b021
Abigail <cinco.de.abby@gmail.com>
2023-09-20 06:50:03 -0400

    filled in the readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # 03-JS-Password-Generator
-Challenge 3 JavaScript Password Generator
+
+## Description
+
+This is an application created to generate passwords depending on criteria input by the user. The user can choose if their password is anywhere from 8-128 characters in length and includes uppercase letters, lowercase letters, numbers, or special characters.
+
+## Table of Contents
+[Screenshots](#sceenshots)
+[Credit]
+
+## Screenshots of Application
+
+## Credit
+
+Base code provided by UofT Coding Bootcamp
+
+## Installation
+
+N/A
+
+## License
+
+License in repo
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '1234567890';
-const special = '!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~';
+const special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
 // Write password to the #password input
@@ -17,6 +17,67 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+function generatePassword() {
+  
+  //password length check
+
+var passLength = prompt("How long do you want your password to be? Input a number from 8 to 128.");
+
+
+console.log(passLength);
+
+if (passLength < 8) {
+  alert("Password is too short! Enter a number equal to or greater than 8.");
+  return" ";
+}
+if (passLength > 128) {
+  alert("Password is too long! Enter a number equal to or less than 128.");
+  return " ";
+}
+if (isNaN(passLength)) {
+  alert("Please enter a valid number between 8 and 128.")
+}
+
+//check criteria
+
+var checkUpper = window.confirm("Do you want to include uppercase letters in your password?");
+var checkLower = window.confirm("Do you want to include lowercase letters in your password?");
+var checkNumbers = window.confirm("Do you want to include numbers in your password?");
+var checkSpecial = window.confirm("Do you want to include special characters in your password?");
+
+//check if at least one character type is selected
+
+valid = false;
+while (valid === false){
+  if(checkUpper === false && checkLower === false && checkNumbers === false && checkSpecial === false) {
+    alert("Please confirm at least one character case and try again.");
+    var checkUpper = window.confirm("Do you want to include uppercase letters in your password?");
+var checkLower = window.confirm("Do you want to include lowercase letters in your password?");
+var checkNumbers = window.confirm("Do you want to include numbers in your password?");
+var checkSpecial = window.confirm("Do you want to include special characters in your password?");
+  }
+  else{
+    valid=true;
+  }
+}
+
+
+//generate password
+
+var charSet = '';
+if (checkUpper) charSet+=upperCase;
+if (checkLower) charSet+=lowerCase;
+if (checkNumbers) charSet+= numbers;
+if (checkSpecial) charSet+= special;
+
+var password = '';
+for (i = 0; i < passLength; i++) {
+  var randomSet = Math.floor(Math.random() * charSet.length);
+  password += charSet.charAt(randomSet);
+}
+return password;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
